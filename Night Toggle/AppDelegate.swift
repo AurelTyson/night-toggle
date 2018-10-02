54af3a85aad8ea8f96cfb26f6407f6507694895a
Aurel Tyson <aurel.tis@gmail.com>
2018-10-02 20:28:54 +0200

    🚀 Init project

---
+++ Night Toggle/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Night Toggle
+//
+//  Created by Aurélien Tison on 02/10/2018.
+//  Copyright © 2018 Aurel Tyson. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
