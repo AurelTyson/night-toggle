1a4bd9a4788135aa7d5f8b533782a332d728e43b
Aurel Tyson <aurel.tis@gmail.com>
2018-10-02 22:21:52 +0200

    Add tooltip view controller

--- Night Toggle/AppDelegate.swift
+++ Night Toggle/AppDelegate.swift
@@ -9,45 +9,60 @@
 import Cocoa
 
 @NSApplicationMain
-class AppDelegate: NSObject, NSApplicationDelegate {
-
-    @IBOutlet weak var window: NSWindow!
+public final class AppDelegate: NSObject, NSApplicationDelegate {
     
-    let statusItem = NSStatusBar.system.statusItem(withLength:NSStatusItem.squareLength)
+    // MARK: Graphic attributes
+    
+    @IBOutlet public weak var window: NSWindow!
+    
+    // MARK: Attributes
+    
+    private let popover = NSPopover()
+    private let statusItem = NSStatusBar.system.statusItem(withLength:NSStatusItem.squareLength)
+    private var eventMonitor: EventMonitor?
+    
+    // MARK: AppDelegate
 
-
-
-    func applicationDidFinishLaunching(_ aNotification: Notification) {
+    public func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
         
         if let button = statusItem.button {
             button.image = NSImage(named:NSImage.Name("StatusBarButtonImage"))
-            button.action = #selector(printQuote(_:))
+            button.action = #selector(togglePopover(_:))
         }
         
-        constructMenu()
+        self.popover.contentViewController = TooltipViewController()
         
-    }
+        self.eventMonitor = EventMonitor(mask: [.leftMouseDown, .rightMouseDown]) { [weak self] event in
+            if let strongSelf = self, strongSelf.popover.isShown {
+                strongSelf.closePopover(sender: event)
+            }
+        }
 
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
-    }
-
-    @objc func printQuote(_ sender: Any?) {
-        let quoteText = "Never put off until tomorrow what you can do the day after tomorrow."
-        let quoteAuthor = "Mark Twain"
-        
-        print("\(quoteText) — \(quoteAuthor)")
     }
     
-    func constructMenu() {
-        let menu = NSMenu()
+    public func applicationWillTerminate(_ aNotification: Notification) {
         
-        menu.addItem(NSMenuItem(title: "Print Quote", action: #selector(AppDelegate.printQuote(_:)), keyEquivalent: "P"))
-        menu.addItem(NSMenuItem.separator())
-        menu.addItem(NSMenuItem(title: "Quit Quotes", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q"))
-        
-        statusItem.menu = menu
     }
-
+    
+    @objc func togglePopover(_ sender: Any?) {
+        if popover.isShown {
+            closePopover(sender: sender)
+        } else {
+            showPopover(sender: sender)
+        }
+    }
+    
+    func showPopover(sender: Any?) {
+        if let button = statusItem.button {
+            popover.show(relativeTo: button.bounds, of: button, preferredEdge: NSRectEdge.minY)
+        }
+        eventMonitor?.start()
+    }
+    
+    func closePopover(sender: Any?) {
+        popover.performClose(sender)
+        eventMonitor?.stop()
+    }
+    
 }
---
+++ Podfile
@@ -0,0 +1,17 @@
+# Uncomment the next line to define a global platform for your project
+platform :osx, '10.14'
+
+target 'Night Toggle' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Night Toggle
+  pod 'RxCocoa'
+  pod 'RxSwift'
+
+  target 'Night ToggleTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
